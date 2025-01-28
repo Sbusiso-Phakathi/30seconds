93addccbb340755fcf9c45f8aa062481511a810d
Phaks323@gmail.com <phaks323@gmail.com@gmail.com>
2025-01-28 10:52:00 +0200

    FIRST PUSH

---
+++ events.session.sql
@@ -0,0 +1,16 @@
+-- CREATE TABLE teams(
+--     teamid SERIAL PRIMARY KEY,
+--     team VARCHAR
+-- )
+
+-- CREATE TABLE players(
+--     playerid SERIAL PRIMARY KEY,
+--     player VARCHAR,
+--     teamid INTEGER,
+--     FOREIGN KEY (teamid) REFERENCES teams(teamid)
+-- )
+
+-- CREATE TABLE cards(
+--     cardid SERIAL PRIMARY KEY,
+--     card TEXT[]
+-- );
